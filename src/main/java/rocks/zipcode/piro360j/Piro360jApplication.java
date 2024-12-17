70e2856f7febbf08e39ed577837f77d71cb7d50f
Kristofer <kris@youngers.org>
2024-12-17 14:54:16 -0500

    adds Spring Boot initializr changes

---
+++ src/main/java/rocks/zipcode/piro360j/Piro360jApplication.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.piro360j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Piro360jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Piro360jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocks/zipcode/piro360j/Piro360jApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.piro360j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Piro360jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
